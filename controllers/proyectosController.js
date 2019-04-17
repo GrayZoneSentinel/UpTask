9a0bb4b0a3fbb4b03ea349e6299adda22a898c40
Ernesto García Balda <grayzonesentinel@MacBook-Pro-de-Ernesto.local>
2019-04-17 19:48:38 +0200

    Finish Create and Read projects

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -3,20 +3,28 @@ const Proyectos = require('../models/Proyectos');
 // Importar Slug to retrieve the Project's URL
 const slug = require('slug');
 
-exports.proyectosHome = (req, res) => {
+exports.proyectosHome = async (req, res) => {
+    // Get los distintos proyectos
+    const proyectos = await Proyectos.findAll();
     res.render('index', {
-        nombrePagina: 'UPTASK - Proyectos'
+        nombrePagina: 'UPTASK - Proyectos ' + res.locals.year,
+        proyectos
     });
 }
 
-exports.formularioProyecto = (req, res) => {
+exports.formularioProyecto = async (req, res) => {
+    const proyectos = await Proyectos.findAll();
+
     res.render('nuevo-proyecto', {
-        nombrePagina: 'UPTASK - Crear proyecto'
+        nombrePagina: 'UPTASK - Crear proyecto',
+        proyectos
     });
 }
 
 // exports.nuevoProyecto = (req, res) => {
 exports.nuevoProyecto = async (req, res) => {
+    const proyectos = await Proyectos.findAll();
+
     // res.send('Proyecto registrado correctamente')
     // Recuperar lo que el usuario escribe en el textfield
     console.log(req.body)
@@ -31,7 +39,8 @@ exports.nuevoProyecto = async (req, res) => {
     if(errores.length > 0){
         res.render('nuevo-proyecto', {
             nombrePagina: 'Nuevo proyecto',
-            errores
+            errores,
+            proyectos
         })
     } else {
         // No hay error
@@ -49,4 +58,25 @@ exports.nuevoProyecto = async (req, res) => {
         const proyecto = await Proyectos.create({ nombre });
         res.redirect('/');
     }
+}
+
+// Proyectos por URL: listar proyectos en la sidebar izq
+exports.proyectoPorUrl = async (req, res, next) => {
+    // res.send('Listo');
+    const proyectos = await Proyectos.findAll();
+
+    const proyecto = await Proyectos.findOne({
+        where: {
+            url: req.params.url
+        }
+    });
+    if(!proyecto) return next();
+    // Render a la vista
+        // console.log(proyecto);
+        // res.send('Proyecto sended OK');
+    res.render('tareas', {
+        nombrePagina: 'Tareas del proyecto',
+        proyecto,
+        proyectos
+    })
 }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // Importar el archivo de las rutas
 const routes = require('./routes');
+// Importar Helpers file
+const helpers = require('./helpers');
 // Conexión a la base de datos
 const db = require('./config/db');
                     // db.authenticate().then(()=>console.log('Conectado a la base de datos correctamente')).catch(error => console.log(error));
@@ -20,6 +22,17 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 //Añadir la carpeta de las vistas
 app.set('views', path.join(__dirname, './views'));
+// Pasar var dump a la aplicación
+app.use((req, res, next)=>{
+        res.locals.vardump = helpers.vardump;
+        next();
+})
+// APRENDIENDO MIDDLEWARE
+app.use((req, res, next) =>{
+    const fecha = new Date();
+    res.locals.year = fecha.getFullYear();
+    next();  
+})
 // Habilitar bodyparser para ver lo que se escribe en el textfields
 app.use(bodyParser.urlencoded({extended:true}));
 // Configurar ruta acceso index
